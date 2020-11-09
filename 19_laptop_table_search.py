e1640f120920eaddf320a9b53180aa78bb13ff34
thghu123 <thghu@naver.com>
2020-11-10 00:46:39 +0900

    cpu/gpu_bench crawler 구현 부

--- 19_laptop_table_search.py
+++ 19_laptop_table_search.py
@@ -9,11 +9,11 @@ cursor.execute(sql) #laptopOS의 이름이 너무 길다 maximum:20
 
 for row in cursor:
     for i in range(len(row)):
-        if i==3:
-            if(row[3] != None):
-                description = row[3].read()
+        #if i==3:
+            #if(row[3] != None):
+                #description = row[3].read()
         print(row[i],end="  ")
-    print("")
+    print()
 
 cursor.close()
 db.close()
